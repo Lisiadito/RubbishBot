6d310f555ef3a238c92d6b3e1f2053c5ad74b856
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2019-09-29 13:49:59 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rubbish Bot
+
+A bot which notifies you when it's time to bring out the rubbish. 
+Needs a `.ical` file in the same folder then `index.js` with the times the rubbish collection comes to pick up the rubbish.
+Needs a `.env` file with a `TELEGRAM_API_KEY`.
+
---
+++ index.js
@@ -0,0 +1,41 @@
+process.env['NTBA_FIX_319'] = 1;
+require('dotenv').config()
+
+const TelegramBot = require('node-telegram-bot-api')
+const bot = new TelegramBot(process.env.TELEGRAM_API_KEY, {polling: true})
+const ical = require('node-ical')
+const dayjs = require('dayjs')
+const locale = require('dayjs/locale/de')
+const schedule = require('node-schedule')
+
+dayjs.locale(locale)
+
+let data = ical.parseFile('muellkalender.ics')
+let chatID
+
+function sendRubbishMessage() {
+	data = Object.values(data).filter(date => dayjs(date.start).diff(dayjs(), 'hour') >= 0)
+	if (data.length <= 0) {
+	  	bot.sendMessage(chatID, `Kalendar enthält keine aktuellen Daten mehr. Downloaden den aktuellen.`)	
+	} else {
+		data.forEach(date => {
+			if (dayjs(date.start).diff(dayjs(), 'hour') <= 24) {
+		  		bot.sendMessage(chatID, `Müll rausbringen. ${date.summary.match(/- (.*)/)[1]} wird am ${dayjs(date.start).format('dddd DD.MM.YYYY')} abgeholt.`)
+			}
+		})
+	}
+}
+
+bot.onText(/\/start/, msg => {
+    chatID = msg.chat.id;
+	bot.sendMessage(chatID, `Bot neustarten. Der Bot checkt jeden Morgen um 7:00 und Abends um 19:00 ob Müll rausgebracht werden muss.`)
+	schedule.scheduleJob('0 7,19 * * *', sendRubbishMessage)	
+})
+
+bot.onText(/\/active/, msg => {
+	msg.chat.id === chatID ? bot.sendMessage(chatID, 'Bot läuft in diesem Channel') : bot.sendMessage(msg.chat.id, 'Bot läuft in einem anderen Channel. Um hier zu verwenden /start tippen.') 
+})
+
+
+
+
